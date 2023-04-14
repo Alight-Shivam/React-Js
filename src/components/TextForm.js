c6b4c16f3eae846ffdf861727351eb5490ec56cf
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-04-14 14:34:58 +0530

    React Js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,6 +10,10 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText)
     }
+    const handleClearClick = ()=>{
+        let newText = '';
+        setText(newText)
+    }
     const handleOnChange = (event)=>{
         setText(event.target.value); 
     }
@@ -23,11 +27,12 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To Upper-Case</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To Lower-Case</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
         </div>
         <div className="container my-3">
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} minute s reading Time</p>
+            <p>{0.008 * text.split(" ").length} minutes reading Time</p>
             <h2>Preview</h2>
             <p>{text}</p>
         </div>
