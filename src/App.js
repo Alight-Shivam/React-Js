8662019b53e7fd333945954228979cddaeafae18
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-05-06 11:57:51 +0530

    React Js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Switch from 'react';
 import Alert from './components/Alert';
-
-// import About from './components/About';
-
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
@@ -26,24 +31,36 @@ const showAlert = (message, type)=>{
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
       showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+
     }
   }
   return (
     <>
+  <Router>
 <Navbar title= "TextUtils" mode={mode} toggleMode={toggleMode} />
 <Alert alert = {alert}/>
 <div className="container my-3">
-<TextForm showAlert ={showAlert} heading = "Enter the Text to Analyze Below"/>
-{/* <About/> */}
+  <Switch>
+    <Route exact path="/about">
+      <About />
+    </Route>
+    <Route path="/">
+    <TextForm exact showAlert ={showAlert} heading = "Enter the Text to Analyze Below"/>   
+    </Route>
+  </Switch>
 </div>
+  </Router>
 
     </>
   );
 }
 
 export default App;
+
